0325673f0157ed03d1609530f20871242ff997ef
Gaurav Agarwal <bharatmicrosystems@gmail.com>
2021-05-10 14:18:53 +0530

    Initial commit

--- provider.tf
+++ provider.tf
@@ -3,3 +3,10 @@ provider "google" {
   region      = "us-central1"
   zone        = "us-central1-c"
 }
+
+terraform {
+  backend "gcs" {
+    bucket  = "tf-state-gitops-terraform-${var.project_id}"
+    prefix  = "gitops-terraform"
+  }
+}
